f54d18ede82e19a4ed9e647ece3e76e16b34afda
MitSeven <nguyenphuongtien@outlook.com>
2018-12-07 19:31:30 +0700

    testONOFF

---
+++ sketch_dec07a/sketch_dec07a.ino
@@ -0,0 +1,22 @@
+int val;
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    while(Serial.available()){
+      val=Serial.read();
+      Serial.println(val);
+      delay(200);
+    }
+    if(val=='1'){
+      digitalWrite(led,LOW);
+    }
+    else if (val=='2'){
+      digitalWrite(led,HIGH);
+    }
+}
